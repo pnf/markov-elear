68bfb24360406fab6dfb47e982e5d00308359157
Peter Fraenkel <pnf@podsnap.com>
2018-05-18 16:54:58 -0400

    Update deps

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [overtone/at-at "1.2.0"]
-                 [twitter-api "0.7.8"]
-                 [environ "1.0.0"]]
+                 [twitter-api "1.8.0"]
+                 [environ "1.1.0"]]
   :main markov-elear.generator
-  :min-lein-version "2.0.0"
-  :plugins [[lein-environ "1.0.0"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.7.0"]]}})
+  :plugins [[lein-environ "1.1.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]]}})
